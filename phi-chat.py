35138b63766102d4943c8bdc7ffecb244a31fb61
suramyavns <sdidwania645@gmail.com>
2024-10-24 01:14:52 +0530

    style

--- phi-chat.py
+++ phi-chat.py
@@ -6,7 +6,6 @@ import streamlit as st
 load_dotenv()
 st.set_page_config('PHI-CHAT',page_icon='microsoft.png')
 st.title('PHI-CHAT')
-st.subheader('Chat with Microsoft PHI as much as you want')
 
 client = InferenceClient(api_key=os.getenv('apiKey'))
 
@@ -25,7 +24,7 @@ else:
 
 user_input = st.chat_input('Your message: ')
 if user_input:
-    chat.append(f'You: {user_input}')
+    chat.append(f'{user_input}')
     messages.append({"role": "user", "content": user_input})
     response = []
     for message in client.chat_completion(
@@ -35,8 +34,11 @@ if user_input:
         stream=True,
     ):
         response.append(message.choices[0].delta.content)
-    chat.append(f'PHI: {''.join(response)}')
+    chat.append(f'{''.join(response)}')
     st.session_state.message=message
     st.session_state.chat=chat
-for message in st.session_state.chat:
-    st.write(message)
+for index in range(len(st.session_state.chat)):
+    if index%2==0:
+        st.markdown('**YOU**'+': '+st.session_state.chat[index])
+    else:
+        st.markdown('**PHI:**'+st.session_state.chat[index])
